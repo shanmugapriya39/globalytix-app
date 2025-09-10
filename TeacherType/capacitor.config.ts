895cfbd7fd86438121442490a81e5b645f332323
Shanmuga Priya <shanmugapriya39@gmail.com>
2025-09-10 06:08:32 +0400

    Set Capacitor appId to com.AbleNest

--- TeacherType/capacitor.config.ts
+++ TeacherType/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.globalytix.app',
+  appId: 'com.AbleNest',
   appName: 'Globalytix',
   webDir: 'dist/public',
   server: {
